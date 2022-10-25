7a1663d8e5d48a1eb8263ad0e81330387b3ad16d
IloveNooodles <mgarebaldhie80@gmail.com>
2022-10-25 18:43:30 +0700

    feat: add register controller

---
+++ src/interface/controller/register_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+class Register extends Controller {
+    public function index(){
+      switch($_SERVER["REQUEST_METHOD"]){
+        case "GET":
+          $this->view("register/index.php");
+          break;
+        case "POST":
+          $this->register();
+          break;
+      }
+    }
+
+    /* Check for req body first return json empty harusnya */ 
+    public function register(){
+      if(!(isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['isAdmin']))) {
+
+      }
+    }
+}
+
+?> 
