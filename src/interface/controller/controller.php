d8a2cb8a69d933ae3a14393f690535937721a99d
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2022-10-26 13:07:17 +0700

    feat: add ui insert album

--- src/interface/controller/controller.php
+++ src/interface/controller/controller.php
@@ -4,8 +4,6 @@ define("VIEW_DIR", $_ENV['PWD'] . '/src/view/');
 
 class Controller {
     public function view($view, $data = []) {
-        // print_r($data);
         require_once VIEW_DIR . $view . '.php';
-        return_songlist($data);
     }
 }
