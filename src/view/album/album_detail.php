89f9ad272d9d933dc06df7593e9b540621b6c7de
arikrayi <arikrayi@gmail.com>
2022-10-27 20:59:47 +0700

    feat: add album detail fe

---
+++ src/view/album/album_detail.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Binotify</title>
+  <link rel="stylesheet" href="/public/css/styles.css">
+  <link rel="stylesheet" href="/public/css/navbar.css">
+  <link rel="stylesheet" href="/public/css/songList.css">
+  <link rel="stylesheet" href="/public/css/albumDetail.css">
+  <link rel="stylesheet" href="/public/css/songDetail.css">
+  <link rel="shortcut icon" href="/public/img/favicon.png" type="image/x-icon">
+</head>
+<body>
+  <?php include_once 'src/view/component/navbar.php' ?>
+  <div class="album-details">
+    <img class="album-image" src=<?=str_replace(BASE_URL,"",$data["album"]["image_path"])?> alt="album1">
+    <div class="title-artist">
+      <p class="album-title"><?=$data["album"]["judul"]?></p>
+      <p class="album-artist"><?=$data["album"]["penyanyi"]?></p>
+      <p class="album-duration">
+        <?php
+          $min = intdiv($data["album"]["total_duration"],60);
+          $sec = fmod($data["album"]["total_duration"],60);
+          echo ($min == 1 ? $min . " minute " : $min . " minutes ") . ($sec == 1 ? $sec . " second" : $sec . " seconds");
+        ?>
+      </p>
+    </div>
+  </div>
+  <?php 
+  include_once 'src/view/component/song_list.php';
+  return_html($data['songs'], true);
+  ?>
+  <?php include_once 'src/view/component/song_detail.php' ?>
+</body>
+</html>
--- src/view/component/album_list.php
+++ src/view/component/album_list.php
@@ -23,11 +23,13 @@ function albums_in_html($data){
             $genre = $album['genre'];
             $html = <<<"EOT"
             <li class="album-list-item" name="$album_id">
-                <img class="album-image" src="$image_path" alt="album1">
-                <div class="album-text">
-                    <p class="album-song-title">$judul</p>
-                    <p class="album-song-artist">$tanggal_terbit | $penyanyi | $genre</p>
-                </div>
+                <a href="/album/detail?id=$album_id">
+                    <img class="album-image" src="$image_path" alt="album1">
+                    <div class="album-text">
+                        <p class="album-song-title">$judul</p>
+                        <p class="album-song-artist">$tanggal_terbit | $penyanyi | $genre</p>
+                    </div>
+                </a>
             </li>
     EOT;
             $str = $str . $html;
