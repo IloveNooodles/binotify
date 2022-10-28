4e11ae76f6164a319c959492f1432b702cef6808
arikrayi <arikrayi@gmail.com>
2022-10-28 11:43:35 +0700

    style: update album styling and remove comments

--- src/view/album/index.php
+++ src/view/album/index.php
@@ -19,12 +19,5 @@
       return_html($data);  
     ?>
   </div>
-  <!-- <script>
-    document.getElementById("album-order-btn").addEventListener("click", function() {
-      document.querySelector(".sort-menu").classList.toggle("show");
-      document.querySelector("#album-order-btn .arrow").classList.toggle("down");
-      document.querySelector("#album-order-btn .arrow").classList.toggle("up");
-    });
-  </script> -->
 </body>
 </html>
