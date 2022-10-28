f098965d63004a33e90b4630d26f9c24f729fe76
IloveNooodles <mgarebaldhie80@gmail.com>
2022-10-28 14:41:13 +0700

    feat: adding middleware expire session

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 require_once $_ENV['PWD'] . '/src/init.php';
 if (!session_id() || session_status() == 0) {
   session_start();
+  if (isset($_SESSION['time']) && (time() - $_SESSION['time'] > 60 * 60 * 24)) {
+    session_unset();     
+    session_destroy();   
+  }
+  $_SESSION['time'] = time();
   $_SESSION['num_song_played'] = 0;
 };
 $app = new App();
