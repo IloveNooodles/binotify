6057c7af258942d31ccf99cde2a24db790e5c526
IloveNooodles <mgarebaldhie80@gmail.com>
2022-10-28 09:25:12 +0700

    limit song"

--- public/js/song-detail.js
+++ public/js/song-detail.js
@@ -4,50 +4,69 @@ const musicDuration = document.querySelector(".song-duration");
 const playBtn = document.querySelector(".play-btn");
 const currentTime = document.querySelector(".current-time");
 
+const xhr_limit_song = new XMLHttpRequest();
 
 const setMusic = () => {
-    seekBar.value = 0;
-    currentTime.innerHTML = '00 : 00';
-    setTimeout(() => {
-        seekBar.max = music.duration;
-        musicDuration.innerHTML = formatTime(music.duration);
-    }, 200);
-}
+  seekBar.value = 0;
+  currentTime.innerHTML = "00 : 00";
+  setTimeout(() => {
+    seekBar.max = music.duration;
+    musicDuration.innerHTML = formatTime(music.duration);
+  }, 200);
+};
 
 setMusic();
 
 if (playBtn.getAttribute("listener") != "true") {
-    playBtn.setAttribute("listener", "true");
-    playBtn.addEventListener('click', () => {
-        if(playBtn.className.includes('pause')){
-            music.play();
-        } else{
-            music.pause();
+  playBtn.setAttribute("listener", "true");
+  playBtn.addEventListener("click", () => {
+    const url = `song/play_song`;
+    xhr_limit_song.open("GET", url);
+    xhr_limit_song.send();
+
+    xhr_limit_song.onreadystatechange = function () {
+      if (xhr_limit_song.readyState == 4 && xhr_limit_song.status == 200) {
+        console.log(xhr_limit_song.responseText);
+        is_limit_song = JSON.parse(xhr_limit_song.responseText);
+        console.log(is_limit_song);
+        is_limit_song = is_limit_song["data"]["can_access"];
+        console.log(is_limit_song);
+        if (is_limit_song) {
+          music.pause();
+          return;
         }
-        playBtn.classList.toggle('pause');
-    })
+
+        if (playBtn.className.includes("pause")) {
+          music.play();
+        } else {
+          music.pause();
+        }
+        playBtn.classList.toggle("pause");
+      }
+    };
+  });
 }
 
 const formatTime = (time) => {
-    let min = Math.floor(time / 60);
-    if(min < 10){
-        min = `0${min}`;
-    }
-    let sec = Math.floor(time % 60);
-    if(sec < 10){
-        sec = `0${sec}`;
-    }
-    return `${min} : ${sec}`;
-}
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min} : ${sec}`;
+};
 
 setInterval(() => {
-    seekBar.value = music.currentTime;
-    currentTime.innerHTML = formatTime(music.currentTime);
-}, 400)
+  seekBar.value = music.currentTime;
+  currentTime.innerHTML = formatTime(music.currentTime);
+}, 400);
 
-seekBar.addEventListener('change', () => {
-    music.currentTime = seekBar.value;
-})
+seekBar.addEventListener("change", () => {
+  music.currentTime = seekBar.value;
+});
 
-    music.play();
-    playBtn.classList.remove('pause');
+// music.play();
+// playBtn.classList.remove("pause");
