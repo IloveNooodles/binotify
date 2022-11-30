1b7edc31b3ea91c8a5c910a4768ddf79f15f5d18
IloveNooodles <mgarebaldhie80@gmail.com>
2022-11-30 22:28:59 +0700

    feat: update and insert subscribe

--- src/interface/controller/subscribed_controller.php
+++ src/interface/controller/subscribed_controller.php
@@ -1,16 +1,67 @@
 <?php
 require_once BASE_URL . '/src/middleware/middleware.php';
-
+require_once BASE_URL . '/src/service/subscription/index.php';
 class Subscribed extends Controller {
     public function index() {
-        $middleware = new Middleware();
-        $is_logged_in = $middleware->is_logged_in();
-        $is_admin = $middleware->can_access_admin_page();
-        if (!$is_logged_in || $is_admin) {
-            redirect_home();
+      switch($_SERVER['REQUEST_METHOD']){
+        case "GET":
+          $middleware = new Middleware();
+          $is_logged_in = $middleware->is_logged_in();
+          $is_admin = $middleware->can_access_admin_page();
+          if (!$is_logged_in || $is_admin) {
+              redirect_home();
+              return;
+          }
+          $this->view("subscribed/index");
+          return;
+          break;
+        case "POST":
+            if(empty($_POST['creator_id']) || empty($_POST['subscriber_id'])){
+              return DATA_NOT_COMPLETE;
+            }
+
+            if($_POST['subscriber_id'] < 0){
+              return INVALID_SUBSCRIBER_ID;
+            }
+
+            if($_POST['creator_id'] < 0){
+              return INVALID_CREATOR_ID;
+            }
+
+            $subscription_service = new SubscriptionService();
+            $res = $subscription_service->subscribe($_POST['creator_id'], $_POST['subscriber_id']);
+            return $res;
+            break;
+        default:
+            response_not_allowed_method();
             return;
-        }
-        $this->view("subscribed/index");
-        return;
+            break;
+      }
+    }
+
+    public function update(){
+      switch($_SERVER['REQUEST_METHOD']){
+        case "POST":
+            if(empty($_POST['creator_id']) || empty($_POST['subscriber_id']) || empty($_POST['status'])){
+              return DATA_NOT_COMPLETE;
+            }
+
+            if($_POST['subscriber_id'] < 0){
+              return INVALID_SUBSCRIBER_ID;
+            }
+
+            if($_POST['creator_id'] < 0){
+              return INVALID_CREATOR_ID;
+            }
+
+            if(!in_array($_POST['status'], ["ACCEPTED", "REJECTED"])){
+              return INVALID_STATUS;
+            }
+
+            $subscription_service = new SubscriptionService();
+            $res = $subscription_service->updateSubscription($_POST['creator_id'], $_POST['subscriber_id'], $_POST['status']);
+            return $res;
+            break;
+      }
     }
 }
---
+++ src/service/subscription/index.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once BASE_URL . '/src/interface/model/subscription.php';
+
+class SubscriptionService {
+    public function get_all_subscription(){
+        $subscription_model = new SubscriptionModel();
+
+        $data = $subscription_model->get_all_subscription();
+        return $data;
+    }
+
+    public function get_subscription_by_subscriber_id($subscriber_id){
+        $subscription_model = new SubscriptionModel();
+
+        $data = $subscription_model->get_all_subscription_by_subscriber_id($subscriber_id);
+        return $data;
+    }
+
+    public function get_subscription_by_id($creator_id, $subscriber_id){
+        $subscription_model = new SubscriptionModel();
+
+        $data = $subscription_model->get_subscription_by_id($creator_id, $subscriber_id);
+        return $data;
+    }
+
+    public function subscribe($creator_id, $subscriber_id){
+        $subscription_model = new SubscriptionModel();
+        try {
+          $subscription = $subscription_model->get_subscription_by_id($creator_id, $subscriber_id);
+          
+          if(isset($subscription)){
+            return SUBSCRIPTION_ALREADY_EXISTS;
+          }
+
+          $subscription_model->insert_subscription($creator_id, $subscriber_id);
+          return SUBSCRIPTION_SUCCESSFULY_CREATED;
+
+        } catch (Exception $e){
+          return INTERNAL_ERROR;
+        }
+    }
+
+    public function updateSubscription($creator_id, $subscriber_id, $status){
+        $subscription_model = new SubscriptionModel();
+        try {
+           $subscription = $subscription_model->get_subscription_by_id($creator_id, $subscriber_id);
+
+          if(!isset($subscription)){
+            return SUBSCRIPTION_NOT_FOUND;
+          }
+
+          $current_status = $subscription['status'];
+
+          if($current_status === 'REJECTED'){
+            return SUBSCRIPTION_ALREADY_REJECTED;
+          }
+
+          if($current_status === 'ACCEPTED'){
+            return SUBSCRIPTION_ALREADY_ACCEPTED;
+          }
+
+          $subscription_model->update_subscription($creator_id, $subscriber_id, $status);
+
+          if($status === 'ACCEPTED'){
+            return SUBSCRIPTION_ACCEPTED;
+          }
+
+          if($status === 'REJECTED'){
+            return SUBSCRIPTION_REJECTED;
+          }
+          
+        } catch (Exception $e){
+          return INTERNAL_ERROR;
+        }
+    }
+}
