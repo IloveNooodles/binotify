08e365e080de42737062c221e190c95cf29e0f24
arikrayi <arikrayi@gmail.com>
2022-11-30 19:33:17 +0700

    fix: disable admin can access premium songs page

--- src/interface/controller/subscribed_controller.php
+++ src/interface/controller/subscribed_controller.php
@@ -5,7 +5,8 @@ class Subscribed extends Controller {
     public function index() {
         $middleware = new Middleware();
         $is_logged_in = $middleware->is_logged_in();
-        if (!$is_logged_in) {
+        $is_admin = $middleware->can_access_admin_page();
+        if (!$is_logged_in || $is_admin) {
             redirect_home();
             return;
         }
